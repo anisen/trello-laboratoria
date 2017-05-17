4a39eb9ccc7a94f2b29ab2a3b93ef1b0f9461c17
anisen <ananavelez@gmail.com>
2017-05-17 16:53:18 -0400

    tratando de que imprima

--- js/trello.js
+++ js/trello.js
@@ -10,16 +10,24 @@ document.getElementById("anadir-tarea").addEventListener("click",function (){
 		var lista=document.getElementById("nameList");
 		lista.innerHTML=nombreLista;
 		//tratando de que me deje el texto del input y me remueva el input y el boton pero no me sale nada :c
+		
 		/*document.getElementById("botonguardar").removeEventListener("click",function(){
 			document.getElementById("anadir-tarea");
 		})*/
+
+		//creacion del input para el listado y el boton del respectivo.
       lista.addEventListener("mouseover",function(){
-      	var input2= "<input type=text id=input2 placeholder='Añade elementos a tu lista'>";
+      	var input2= "<input type=text id=input22 placeholder='Añade elementos a tu lista'>";
       	var primeLista=document.getElementById("primera-lista");
       	var botondeLista="<button id=botonParaAgregarALaLista>Añadir tarjeta</button>"
-      	primeLista.innerHTML=input2+botondeLista;
+        primeLista.innerHTML=input2+botondeLista;
 
-      })
+        document.getElementById("botonParaAgregarALaLista").addEventListener("click",function(){ 
+        var acomulador=document.getElementsByClassName("informacion");
+       acomulador[0].innerHTML +="<li>"+document.getElementById("input22").value+"</li>";
+         });
+       });
+       
       
 	})
 
